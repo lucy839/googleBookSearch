8c60ac6063d6ca5c66ac27c82b2f8588025a1c99
Youngju Lee <lucylee839@hotmail.com>
2019-07-14 14:47:43 -0700

    comment included

--- client/src/components/SearchForm/index.js
+++ client/src/components/SearchForm/index.js
@@ -1,27 +1,26 @@
 import React from "react";
 import { Input, FormBtn } from "../Form";
 
-// import "./style.css";
-
+// Form used to search
 const SearchForm = props => {
-    return (
-        <form>
-            <div className="form-group">
-                <label className="BookSearchLabel"><h3>Search For Book</h3></label>
-                <br></br>
-                <Input
-                value={props.search}
-                onChange={props.handleInputChange}
-                name="search"
-                placeholder="Enter Book's Name"
-              />
-            </div>
-            <FormBtn
-                onClick={props.handleFormSubmit}
-              >
-                Submit Book
+  return (
+    <form>
+      <div className="form-group">
+        <label className="BookSearchLabel"><h3>Search For Book</h3></label>
+        <br></br>
+        <Input
+          value={props.search}
+          onChange={props.handleInputChange}
+          name="search"
+          placeholder="Enter Book's Name"
+        />
+      </div>
+      <FormBtn
+        onClick={props.handleFormSubmit}
+      >
+        Submit Book
               </FormBtn>
-        </form>
-    )
+    </form>
+  )
 }
 export default SearchForm
