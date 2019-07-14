34756a7bdf81faa1328627e228b6fe3fb029e4d5
Youngju Lee <lucylee839@hotmail.com>
2019-07-14 14:59:39 -0700

    added style

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,15 +1,16 @@
 import React from "react";
+import "./style.css";
 
 function Nav() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-      <a className="navbar-brand" href="/">
+      <a className="navbar-brand logo" href="/">
         Google Books
       </a>
-      <a className="navbar-brand" href="/">
+      <a className="navbar-brand" id="link" href="/">
         Search
       </a>
-      <a className="navbar-brand" href="/books">
+      <a className="navbar-brand" id="link" href="/books">
         Saved
       </a>
     </nav>
