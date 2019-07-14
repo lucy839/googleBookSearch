85bddfd1a08557681c85c5141b3c9b208b59bfea
Youngju Lee <lucylee839@hotmail.com>
2019-07-14 14:43:18 -0700

    comment included

--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -4,13 +4,14 @@ import Jumbotron from "../components/Jumbotron";
 import { Container } from "../components/Grid";
 import SavedResults from "../components/SavedResults"
 import Footer from "../components/Footer"
-class Saved extends Component {
-    state = {
-      books: []
-    };
 
-    //when this component mounts, grab all books that were save to the database 
-     componentDidMount() {
+class Saved extends Component {
+  state = {
+    books: []
+  };
+
+  //when this component mounts, grab all books that were save to the database 
+  componentDidMount() {
     this.loadBooks();
   }
 
@@ -22,26 +23,27 @@ class Saved extends Component {
       .catch(err => console.log(err));
   };
 
-    //function to remove book by id
-    handleDeleteButton = id => {
-        API.deleteBook(id)
-            .then(res => this.loadBooks())
-            .catch(err => console.log(err))
-    }
+  // Function to remove book by id
+  handleDeleteButton = id => {
+    API.deleteBook(id)
+      .then(res => this.loadBooks())
+      .catch(err => console.log(err))
+  }
 
-    render() {
-        console.log(this.state.savedBooks)
-        return (
-            <Container fluid className="container">
-                <Jumbotron />
-                <Container>
-                    <h3>Books that You Saved</h3>
-                    <SavedResults savedBooks={this.state.books} handleDeleteButton={this.handleDeleteButton} />
-                </Container>
-                <Footer />
-            </Container>
-        )
-    }
+  // Render the books saved
+  render() {
+    console.log(this.state.savedBooks)
+    return (
+      <Container fluid className="container">
+        <Jumbotron />
+        <Container>
+          <h3>Books that You Saved</h3>
+          <SavedResults savedBooks={this.state.books} handleDeleteButton={this.handleDeleteButton} />
+        </Container>
+        <Footer />
+      </Container>
+    )
+  }
 }
 
 
