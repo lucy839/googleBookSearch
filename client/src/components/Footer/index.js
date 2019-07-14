7f03e1c9cd28cc2cc2b1e5f8ac20688288e1b9bd
Youngju Lee <lucylee839@hotmail.com>
2019-07-14 15:04:21 -0700

    finalized

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,12 +1,12 @@
 import React from "react";
-// import "./style.css";
+import "./style.css";
 
 const Footer = () => (
-	<footer className = "page-footer">
-		<div className = "container-fluid text-center">
-			<p>2019 Youngju Lucy Lee | <a href="https://github.com/lucy839/clicky-game">GitHub </a>
-</p>
-		</div>	
+	<footer className="page-footer">
+		<div className="container-fluid text-center">
+			<p id = "footer">2019 Youngju Lucy Lee | <a href="https://github.com/lucy839/clicky-game">GitHub </a>
+			</p>
+		</div>
 	</footer>
 );
 
