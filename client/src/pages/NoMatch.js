f7a3f35f12c613d80b689b078cee81e05ffb9d59
Youngju Lee <lucylee839@hotmail.com>
2019-07-14 14:43:40 -0700

    comment included

--- client/src/pages/NoMatch.js
+++ client/src/pages/NoMatch.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Col, Row, Container } from "../components/Grid";
 import Jumbotron from "../components/Jumbotron";
 
+// NoMatch page when page wasn't found
 function NoMatch() {
   return (
     <Container fluid>
